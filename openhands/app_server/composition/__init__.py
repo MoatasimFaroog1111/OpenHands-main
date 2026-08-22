cbae24f7bb6064068abab55e734ceaad1a94dc13
MoatasimFaroog1111 <moatasim1111@gmail.com>
2026-08-22 11:45:18 +0300

    refactor: add app server composition package

---
+++ openhands/app_server/composition/__init__.py
@@ -0,0 +1,5 @@
+"""Composition-root helpers for OpenHands app-server dependencies.
+
+Modules in this package construct concrete infrastructure injectors while the
+public configuration facade remains in :mod:`openhands.app_server.config`.
+"""
